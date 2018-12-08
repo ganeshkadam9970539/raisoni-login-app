7f94034a18214c6c46421bab66f220700b1dd2c6
ganeshkadam9970539 <45657194+ganeshkadam9970539@users.noreply.github.com>
2018-12-08 15:15:12 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@
 
 # third-party imports
 from flask import Flask
+import os
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_migrate import Migrate
@@ -18,9 +19,19 @@ login_manager = LoginManager()
 
 
 def create_app(config_name):
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_object(app_config[config_name])
-    app.config.from_pyfile('config.py')
+    if os.getenv('FLASK_CONFIG') == "production":
+        app = Flask(__name__)
+        app.config.update(
+            SECRET_KEY=os.getenv('SECRET_KEY'),
+            SQLALCHEMY_DATABASE_URI=os.getenv('SQLALCHEMY_DATABASE_URI')
+        )
+    else:
+        app = Flask(__name__, instance_relative_config=True)
+        app.config.from_object(app_config[config_name])
+        app.config.from_pyfile('config.py')
+    #app = Flask(__name__, instance_relative_config=True)
+   # app.config.from_object(app_config[config_name])
+    #app.config.from_pyfile('config.py')
     db.init_app(app)
 
     login_manager.init_app(app)
