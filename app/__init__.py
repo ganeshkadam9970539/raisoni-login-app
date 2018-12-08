910f4f38ecf155f83efe90eb0e853c7c63586398
ganeshkadam9970539 <45657194+ganeshkadam9970539@users.noreply.github.com>
2018-12-08 16:27:01 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -29,9 +29,9 @@ def create_app(config_name):
         app = Flask(__name__, instance_relative_config=True)
         app.config.from_object(app_config[config_name])
         app.config.from_pyfile('config.py')
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_object(app_config[config_name])
-    app.config.from_pyfile('config.py')
+    #app = Flask(__name__, instance_relative_config=True)
+    #app.config.from_object(app_config[config_name])
+    #app.config.from_pyfile('config.py')
     db.init_app(app)
 
     login_manager.init_app(app)
